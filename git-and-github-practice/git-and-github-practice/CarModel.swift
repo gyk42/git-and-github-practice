edb94b3a5f2e42a53fe7ef0a94ca19fb683d4dc0
Grace Yu <gyk42@hotmail.com>
2016-12-05 16:29:29 -0500

    Initial commit with Car Class

---
+++ git-and-github-practice/git-and-github-practice/CarModel.swift
@@ -0,0 +1,13 @@
+//
+//  CarModel.swift
+//  git-and-github-practice
+//
+//  Created by Yoon Yu on 12/5/16.
+//  Copyright © 2016 Grace Yu. All rights reserved.
+//
+
+import Foundation
+
+class Car {
+   
+}
